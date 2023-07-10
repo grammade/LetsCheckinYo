c31e7e1f849bc5b7f784a63446d244822798f189
NNorman <cst_dev173@unitedtracotrs.com>
2023-07-10 16:22:49 +0700

    add location mocking feature

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.DevTools.V107.Debugger;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -12,8 +13,11 @@ System.Console.WriteLine("Sit tight, let me do all the work....");
 
 new DriverManager().SetUpDriver(new ChromeConfig());
 var driver = new ChromeDriver();
-
-
+var coord = new Dictionary<string, object>{
+    {"latitude", -6.18432},
+    {"longitude", 106.931903},
+    {"accuracy", 20}
+};
 driver.Navigate().GoToUrl("https://heart.globalservice.co.id/");
 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
 
@@ -25,6 +29,7 @@ username.SendKeys(cred[0]);
 password.SendKeys(cred[1]);
 loginSubmitBtn.Click();
 
+var a = driver.ExecuteCdpCommand("Emulation.setGeolocationOverride", coord);
 driver.Navigate().GoToUrl("https://heart.globalservice.co.id/staff/absensi");
 
 var mood = driver.FindElement(By.CssSelector("[src*='https://heart.globalservice.co.id/assets/img/emoji/3.png']"));
@@ -33,11 +38,11 @@ var submitBtn = driver.FindElement(By.CssSelector("[type*='submit']"));
 
 desc.SendKeys($"{submitBtn.Text} at {Round(DateTime.Now, TimeSpan.FromMinutes(1)).ToString("H:mm")}");
 mood.Click();
+Thread.Sleep(7000);
 
 submitBtn.Click();
 
 Thread.Sleep(3000);
-driver.Quit();
 return;
 
 static DateTime Round(DateTime date, TimeSpan interval) {
